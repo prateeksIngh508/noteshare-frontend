2315900ea7701546390ff62a8fcc0c1ab33fb374
prateek singh <39960381+prateeksIngh508@users.noreply.github.com>
2021-06-03 05:05:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3300";
+const baseURL = "https://noteshare-file.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
